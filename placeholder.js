b27414d66dcbc9d383fcbe2b0d6962b62c270e39
David Björklund <david.bjorklund@gmail.com>
2014-11-12 02:13:16 +0100

    simplify

--- placeholder.js
+++ placeholder.js
@@ -4,26 +4,20 @@ var dom = require('dom-events')
   , isEmpty = function (element) {
       return element.textContent.trim() === '' && element.children.length === 0
     }
-  , update = function (element) {
-      if (isEmpty(element) && document.activeElement !== element) {
-        element.classList.add('placeholder')
-      } else {
-        element.classList.remove('placeholder')
-      }
-    }
   , placeholder = function (element) {
-      new MutationObserver(function () {
-        update(element)
-      }).observe(element, { childList: true })
+      var update = function () {
+        if (isEmpty(element) && document.activeElement !== element) {
+          element.classList.add('placeholder')
+        } else {
+          element.classList.remove('placeholder')
+        }
+      }
 
-      update(element)
-      dom.on(element, 'focus', function () {
-        update(element)
-      })
+      new MutationObserver(update).observe(element, { childList: true })
 
-      dom.on(element, 'blur', function () {
-        update(element)
-      })
+      update()
+      dom.on(element, 'focus', update)
+      dom.on(element, 'blur', update)
     }
 
 require('insert-css')(require('fs').readFileSync(__dirname + '/placeholder.css', 'utf8'))
