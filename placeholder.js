7d46a54cf87e6d685a24029bfd5b622b3a48009f
David Björklund <david.bjorklund@gmail.com>
2015-10-24 23:05:44 +0200

    lint

--- placeholder.js
+++ placeholder.js
@@ -1,29 +1,29 @@
-var dom = require('dom-events')
-  , MutationObserver = require('mutation-observer')
-
-  , isEmpty = function (element) {
-      return element.textContent === '' && element.children.length === 0
-    }
-
-  , placeholder = function (element) {
-      var update = function () {
-        if (isEmpty(element) && document.activeElement !== element) {
-          element.classList.add('placeholder')
-        } else {
-          element.classList.remove('placeholder')
-        }
-      }
-
-      new MutationObserver(update).observe(element, { childList: true })
-
-      update()
-      dom.on(element, 'focus', update)
-      dom.on(element, 'blur', update)
-    }
+var dom = require('dom-events');
+var MutationObserver = require('mutation-observer');
 
 // insert the little css needed, since you can't inline set ':before'
 require('insert-css')(
   require('fs').readFileSync(__dirname + '/placeholder.css', 'utf8')
-)
+);
 
-module.exports = placeholder
+module.exports = placeholder;
+
+function isEmpty (element) {
+  return element.textContent === '' && element.children.length === 0;
+}
+
+function placeholder (element) {
+  var update = function () {
+    if (isEmpty(element) && document.activeElement !== element) {
+      element.classList.add('placeholder');
+    } else {
+      element.classList.remove('placeholder');
+    }
+  };
+
+  new MutationObserver(update).observe(element, { childList: true });
+
+  update();
+  dom.on(element, 'focus', update);
+  dom.on(element, 'blur', update);
+}
--- test.js
+++ test.js
@@ -1,107 +1,107 @@
-var x = require('element')
-  , test = require('tape')
+var x = require('element');
+var test = require('tape');
 
-  , placeholder = require('./placeholder')
+var placeholder = require('./placeholder');
 
 test('empty element, focus & blur', function (t) {
-  var element = x('<p contenteditable data-placeholder="beep"></p>')
-  document.body.appendChild(element)
+  var element = x('<p contenteditable data-placeholder="beep"></p>');
+  document.body.appendChild(element);
 
-  placeholder(element)
+  placeholder(element);
 
-  t.ok(element.classList.contains('placeholder'))
+  t.ok(element.classList.contains('placeholder'));
 
-  element.focus()
+  element.focus();
 
-  t.notOk(element.classList.contains('placeholder'))
+  t.notOk(element.classList.contains('placeholder'));
 
-  element.blur()
+  element.blur();
 
-  t.ok(element.classList.contains('placeholder'))
+  t.ok(element.classList.contains('placeholder'));
 
-  t.end()
-})
+  t.end();
+});
 
 test('element with text', function (t) {
-  var element = x('<p contenteditable data-placeholder="boop">foo</p>')
-  document.body.appendChild(element)
+  var element = x('<p contenteditable data-placeholder="boop">foo</p>');
+  document.body.appendChild(element);
 
-  placeholder(element)
+  placeholder(element);
 
-  t.notOk(element.classList.contains('placeholder'))
+  t.notOk(element.classList.contains('placeholder'));
 
-  t.end()
-})
+  t.end();
+});
 
 test('element with whitespace text', function (t) {
-  var element = x('<p contenteditable data-placeholder="boop">   </p>')
-  document.body.appendChild(element)
+  var element = x('<p contenteditable data-placeholder="boop">   </p>');
+  document.body.appendChild(element);
 
-  placeholder(element)
+  placeholder(element);
 
-  t.notOk(element.classList.contains('placeholder'))
+  t.notOk(element.classList.contains('placeholder'));
 
-  t.end()
-})
+  t.end();
+});
 
 test('element with child', function (t) {
-  var element = x('<p contenteditable data-placeholder="bong"></p>')
-  element.appendChild(x('<img src="http://placekitten.com/g/90/90"/>'))
-  document.body.appendChild(element)
+  var element = x('<p contenteditable data-placeholder="bong"></p>');
+  element.appendChild(x('<img src="http://placekitten.com/g/90/90"/>'));
+  document.body.appendChild(element);
 
-  placeholder(element)
+  placeholder(element);
 
-  t.notOk(element.classList.contains('placeholder'))
+  t.notOk(element.classList.contains('placeholder'));
 
-  t.end()
-})
+  t.end();
+});
 
 test('element appending child', function (t) {
-  var element = x('<p contenteditable data-placeholder="blip"></p>')
-    , child = x('<img src="http://placekitten/g/90/90"/>')
+  var element = x('<p contenteditable data-placeholder="blip"></p>');
+  var child = x('<img src="http://placekitten/g/90/90"/>');
 
-  document.body.appendChild(element)
+  document.body.appendChild(element);
 
-  placeholder(element)
+  placeholder(element);
 
-  element.appendChild(child)
+  element.appendChild(child);
 
   // run in next tick so that the observer can run
   process.nextTick(function () {
-    t.notOk(element.classList.contains('placeholder'))
-    t.end()
-  })
-})
+    t.notOk(element.classList.contains('placeholder'));
+    t.end();
+  });
+});
 
 test('element setting innerHTML', function (t) {
-  var element = x('<p contenteditable data-placeholder="blip"></p>')
+  var element = x('<p contenteditable data-placeholder="blip"></p>');
 
-  document.body.appendChild(element)
+  document.body.appendChild(element);
 
-  placeholder(element)
+  placeholder(element);
 
-  element.innerHTML = 'oh yeah'
+  element.innerHTML = 'oh yeah';
 
   // run in next tick so that the observer can run
   process.nextTick(function () {
-    t.notOk(element.classList.contains('placeholder'))
-    t.end()
-  })
-})
+    t.notOk(element.classList.contains('placeholder'));
+    t.end();
+  });
+});
 
 test('element changing in focus', function (t) {
-  var element = x('<p contenteditable data-placeholder="foo">content</p>')
+  var element = x('<p contenteditable data-placeholder="foo">content</p>');
 
-  document.body.appendChild(element)
+  document.body.appendChild(element);
 
-  placeholder(element)
+  placeholder(element);
 
-  element.focus()
+  element.focus();
 
-  element.innerHTML = ''
+  element.innerHTML = '';
 
   process.nextTick(function () {
-    t.notOk(element.classList.contains('placeholder'))
-    t.end()
-  })
-})
+    t.notOk(element.classList.contains('placeholder'));
+    t.end();
+  });
+});
